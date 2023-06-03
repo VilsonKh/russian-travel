1acd991614c7c2be0ef47dccbfa5be04ec30ec9e
Vilson <khvilson@yandex.ru>
2023-06-03 19:55:56 +0300

    removed extra code, added media queries

---
+++ index.js
@@ -0,0 +1,14 @@
+// (function () {
+//   const path = document.querySelector('.lead__path');
+//   const pathLength = path.getTotalLength();
+//   const allTowns = document.querySelectorAll(".lead__town");
+//   const allPoints = document.querySelectorAll(".point");
+//   const allPointsBg = document.querySelectorAll('.point-bg')
+
+//   console.log(pathLength)
+//   if(pathLength === 1000) {
+//     console.log('1000')
+//   }
+// })();
+
+console.log(document.querySelectorAll(".lead__town"));
